fdd834fefd9c99b969700509fc4c5ce15e3e05b7
jli987 <106000625+jli987@users.noreply.github.com>
2024-05-06 00:14:35 -0700

    Update Header.svelte

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,25 +1,32 @@
 <script>
-  import { username, user } from './user';
+  import { username, user, ws } from './user';
 
   function signout() {
     user.leave();
     username.set('');
+
+    // Send a 'LEAVE' message to the WebSocket server
+    if (ws && ws.readyState === WebSocket.OPEN) {
+
+      ws.close();
+    }  
   }
+
 </script>
 
 <header>
-<h1>🔫💬</h1>
+<h1 style="color: yellow">ConnectSphere</h1>
   {#if $username}
     <div class="user-bio">
       
       <span>Hello <strong>{$username}</strong></span>
-      <img src={`https://avatars.dicebear.com/api/initials/${$username}.svg`} alt="avatar" /> 
+      <img src={`https://api.dicebear.com/8.x/pixel-art/svg?seed=${$username}`} alt="avatar" /> 
     </div>
 
     <button class="signout-button" on:click={signout}>Sign Out</button>
 
     {:else}
 
-      <h3>Gun.js Chat</h3>
+      <h3 style="color: yellow">Chat</h3>
   {/if}
 </header>
